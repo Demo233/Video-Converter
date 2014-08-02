5441e5b23d68aa20834166cf850df7271c46a58d
Kevin C.H. I. <kip@kevinip.com>
2014-08-02 17:21:37 -0400

    MKV only, Filename bug fixed & typo fixed.

--- Converter-MP4-MKV.bat
+++ Converter-MP4-MKV.bat
@@ -1,19 +1,15 @@
 @echo off
+setlocal
 :start
 echo ============================================================================
 echo.
-echo    High Compression Video Converter x264-MKV/MP4
-echo    Version 0.9.2 beta by Kevin C.H.I.
+echo    High Compression Video Converter x264-MKV
+echo    Version 0.9.5 beta by Kevin C.H.I.
 echo.
 echo    NOTES:
 echo.
 echo    - Video resolution is same as source
 echo    - Audio sample rate and channel are same as source
-echo.
-echo    IMPORTANT (Filename bug, will be fixed in the future):
-echo.
-echo    - Please rename your source file to a simple name without space or
-echo      special characters suchs as _ - . and etc.
 echo.
 echo ============================================================================
 echo.
@@ -21,16 +17,16 @@ echo  SETTING
 echo.
 echo  Current Directory : %cd%
 echo.
-set /p source="-STEP 1: Source video file > "
+set /p src="-STEP 1: Source video file > "
 echo.
-set /p output="-STEP 2: Output filename > "
+set /p op="-STEP 2: Output filename > "
 echo.
-set /p extension="-STEP 3: Which format? MP4 or MKV > "
-REM set extension=mkv
+REM set /p extension="-STEP 3: Which format? MP4 or MKV > "
+set extension=mkv
 echo.
 echo  - Bitrate Table ----------------------------------------------------------
-echo  Animation : Low resolution 1000 - 2000 High resolution - Desktop Recording
-echo  Animation :    Low details 1500 - 3500 High details - Anime
+echo  Recording : Low resolution 1000 - 2000 High resolution - Desktop Recording
+echo  Animation :     Low details 500 - 3500 High details - Anime
 echo       Film :        Low VFX 2500 - 4000 High VFX or High Action
 echo     Gaming :          Indie 5000 - 10000 Next-Gen VFX FPS MMO RPG
 echo         4K :               15000 - 30000 Max Details
@@ -41,21 +37,12 @@ echo ===========================================================================
 echo.
 
 set extstate=false
-if /i "%source%" == "" GOTO error
-if /i "%output%" == "" GOTO error
 if /i "%quality%" == "" GOTO error
-if /i "%extension%" == "" GOTO error
-if /i "%extension%" == "mp4" set extstate=true
-if /i "%extension%" == "mkv" set extstate=true
-if "%extstate%"=="true" (
-    GOTO adjustment
-)
-if "%extstate%"=="false" (
-    GOTO error
-)
 
 :adjustment
 set /a quality=(%quality% * 1024) / 1000
+set source="%cd%\%src:"=%"
+set output="%cd%\%op:"=%.%extension%"
 
 GOTO summary
 
@@ -65,8 +52,8 @@ echo ===========================================================================
 echo  SUMMARY
 echo.
 echo      Source file : %source%
-echo          Save as : %output%.%extension%
-echo         Location : %cd%
+echo          Save as : %output%
+echo         Location : %cd%\
 echo.
 echo      Video codec : H.264 MPEG-4 AVC, Average ~%quality%%Kbps,
 echo      Audio codec : AAC MPEG 240Kbps, sample rate and channel same as source
@@ -86,7 +73,7 @@ REM x264.exe --pass 1 --level 4.1 --stats .stats --bitrate %bitrate% --no-mbtree
 
 REM x264.exe --pass 2 --level 4.1 --stats .stats --bitrate %bitrate% --no-mbtree --keyint 24 --min-keyint 2 --threads auto --bframes 3 --me umh --ref 4 --subme 7 --direct auto --sar 1:1 --b-pyramid strict --partitions p8x8,b8x8,i4x4,i8x8 --8x8dct --vbv-bufsize 30000 --vbv-maxrate 38000 --weightb --mixed-refs --mvrange 511 --aud --trellis 1 --analyse all --output "%cd%\%output%.%extension%" "%cd%\%source%"
 
-ffmpeg -loglevel warning -i %cd%\%source% -an -vcodec libx264 -pass 1 -preset veryslow -profile:v high -level 4.1 -threads 0 -b:v %quality%k -x264opts frameref=1:fast_pskip=0:keyint=24:min-keyint=2:me=dia:trellis=1:bframes=3:subme=3:direct=auto:b-pyramid:partitions=none:no-dct-decimate -f rawvideo -y NUL
+ffmpeg -loglevel warning -i %source% -an -vcodec libx264 -pass 1 -preset veryslow -profile:v high -level 4.1 -threads 0 -b:v %quality%k -x264opts frameref=1:fast_pskip=0:keyint=24:min-keyint=2:me=dia:trellis=1:bframes=3:subme=3:direct=auto:b-pyramid:partitions=none:no-dct-decimate -f rawvideo -y NUL
 echo.
 echo ============================================================================
 echo.
@@ -94,7 +81,7 @@ echo  PASS-2 conversion in progress, please wait or press Q to cancel.
 echo.
 echo ============================================================================
 echo.
-ffmpeg -loglevel warning -i %cd%\%source% -strict experimental -c:a aac -b:a 240k -vcodec libx264 -pass 2 -preset veryslow -profile:v high -level 4.1 -threads 0 -b:v %quality%k -x264opts frameref=4:fast_pskip=0:keyint=24:min-keyint=2:me=umh:trellis=1:bframes=3:subme=7:vbv-maxrate=40000:vbv-bufsize=30000:direct=auto:b-pyramid:partitions=p8x8,b8x8,i4x4,i8x8:8x8dct:weightb:mixed-refs:mvrange %cd%\%output%.%extension%
+ffmpeg -loglevel warning -i %source% -strict experimental -c:a aac -b:a 240k -vcodec libx264 -pass 2 -preset veryslow -profile:v high -level 4.1 -threads 0 -b:v %quality%k -x264opts frameref=4:fast_pskip=0:keyint=24:min-keyint=2:me=umh:trellis=1:bframes=3:subme=7:vbv-maxrate=40000:vbv-bufsize=30000:direct=auto:b-pyramid:partitions=p8x8,b8x8,i4x4,i8x8:8x8dct:weightb:mixed-refs:mvrange %output%
 echo.
 
 GOTO completed
@@ -104,14 +91,8 @@ echo.
 :error
 echo  SEE ERROR BELOW:
 echo.
-if "%source%" == "" (
-echo  Please locate the source video file.
-)
-if "%output%" == "" (
-echo  Please type in the output filename.
-)
-if "%extstate%"=="false" (
-echo  Please correct the value for the extension parameter.
+if "%quality%" == "" (
+echo  Please set the quality.
 )
 echo.
 echo ============================================================================
@@ -127,7 +108,7 @@ echo.
 del "ffmpeg2pass-0.log" /q
 del "ffmpeg2pass-0.log.mbtree" /q
 echo  Conversion Completed at %DATE:/=-%@%TIME::=-%
-echo  File location : %cd%\%output%.%extension%
+echo  File location : %output%
 echo.
 pause
 cls
